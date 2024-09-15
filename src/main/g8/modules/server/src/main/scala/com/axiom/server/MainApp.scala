9621981c9604a6ed597d6b9db7a57fcbb383267a
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2024-09-15 17:31:37 -0400

    Update MainApp.scala

--- src/main/g8/modules/server/src/main/scala/com/axiom/server/MainApp.scala
+++ src/main/g8/modules/server/src/main/scala/com/axiom/server/MainApp.scala
@@ -1,46 +1,46 @@
 package com.axiom.server
 
-import com.raquo.laminar.api.L.{*,given}
-import org.scalajs.dom
-import scala.util.Try
-import com.raquo.airstream.ownership.OneTimeOwner
+import zio._
+import zio.http._
+import zio.http.endpoint._
+import zio.http.Middleware.{CorsConfig, cors}
+import zio.http.Header.{AccessControlAllowOrigin, Origin}
+object MainApp extends ZIOAppDefault :
 
-object MainApp {
-  def main(args:Array[String]):Unit = {
-    val containerNode  = dom.document.querySelector("#app") //# refers to the ID name of the html element
+  import com.axiom._, dataimport.dataimportapi._
+  //TODO MAYBE NOT NEEDED
+  val config: CorsConfig =
+  CorsConfig(
+    allowedOrigin = {  //allows origin from vite server to access routes on server
+      case origin if origin == Origin.parse("http://localhost:5173").toOption.get =>
+        Some(AccessControlAllowOrigin.Specific(origin))
+      case _  => None
+    },
+  )
 
-    render(
-        containerNode,
-        Tutorial.staticContent
-    )
-  }
+
+  val routes =
+    Routes(
+      Method.GET / Root -> handler(Response.text("Greetings at your service!!! Arnoldorferdorf!!!xxxxx")),
+      Method.GET / "greet" -> handler { (req: Request) =>
+        val name = req.queryParamToOrElse("name", "World")
+        Response.text(s"Hello $name!")
+        },
+      Method.GET / "patients"   -> handler {
+        import com.axiom._, dataimport._
+        Response.text(s"$importpatients")
+      },
+      Method.GET / "patientsjson"   -> handler {
+        import com.axiom._, dataimport._
+        import com.axiom.model.shared._, dto._
+        import zio.json._
+        Response.text(s"${importpatients.toJson}")
+      }
+    )  @@ cors(config) //cors configuration
+
+  val staticroutes =  Routes.empty @@ Middleware.serveResources(Path.empty / "static") //accesses resources within directories and subdirectories
+  override val run = {
+    Console.printLine("please visit http://localhost:8080") 
+    Server.serve(routes++staticroutes).provide(Server.default)
 }
-
-
-object Tutorial:
-  val ticks = EventStream.periodic(1000)  
-  
-  val staticContent =
-    div (
-      // modifiers
-      styleAttr := "color: blue",
-      p("Hello world!"),
-      p("Hello Beans 🫘🫘🫘🫘"),
-      p("Hello world!"),
-      p("Hello Beans 🫘🫘"),
-      div(span("Time updated: "),
-      child <-- ticks.map(_.toString()))
-    )
-    
-    // timeupdated
-
-
-  val timeupdated = 
-    div (
-      span("Time since updated:"),
-      child <-- ticks.map(_.toString())
-    )
-
-
-
 
